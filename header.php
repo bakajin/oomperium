566432484cb49a5fb545865318b7459d28c75fda
Olivier Oskamp <baka124@hotmail.com>
2014-12-10 18:59:26 +0100

    Menu styling

--- header.php
+++ header.php
@@ -25,13 +25,12 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'oomperium' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-	<!-- OOMP custom header code, defined by inc/custom-header.php in functions.php -->
-	<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>%" width="<?php echo get_custom_header()->width; ?>%" alt="logo" class="header-logo" />
-		<div class="site-branding">
+	<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
-
+	<!-- OOMP custom header code, defined by inc/custom-header.php in functions.php -->
+	<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>%" width="<?php echo get_custom_header()->width; ?>%" alt="logo" class="header-logo" />
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'oomperium' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
